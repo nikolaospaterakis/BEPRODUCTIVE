5af800ace450b46e544f35283b0dbd5ba94b08fe
yu1210 <104638197+nikolaospaterakis@users.noreply.github.com>
2024-06-18 21:33:24 +0300

    Add files via upload

--- components/MyGroup.jsx
+++ components/MyGroup.jsx
@@ -4,8 +4,12 @@ import {select, line, curveCardinal} from "d3";
 export default function MyProductivity(){
 
     return (
-        <section>
+        <section className="yourgroup-content">
             <h1>Your Group</h1>
+            <p>Socialize with other users here</p>
+            <footer>
+                <p>coming soon...</p>
+            </footer>
         </section>
     )
 }
